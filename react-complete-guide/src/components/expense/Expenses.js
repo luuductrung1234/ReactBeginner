048f03ac26a256740470a5f350852fb40c561e14
Trung Luu <luuductrung1234@gmail.com>
2022-05-11 13:44:19 +0700

    add expense component

---
+++ react-complete-guide/src/components/expense/Expenses.js
@@ -0,0 +1,20 @@
+import "./Expenses.css";
+import Card from "../shared/Card";
+import ExpenseItem from "./ExpenseItem";
+
+const Expenses = (props) => {
+  let expensesItems = [];
+  props.expenses.forEach((expense) => {
+    expensesItems.push(
+      <ExpenseItem
+        key={expense.id}
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}
+      />
+    );
+  });
+  return <Card className="expenses">{expensesItems}</Card>;
+};
+
+export default Expenses;
