a2bc845eb15c34343b7730ef29fe2fde52494a64
Trung Luu <luuductrung1234@gmail.com>
2022-05-12 16:47:58 +0700

    add ExpenseFilter

--- react-complete-guide/src/components/expense/Expenses.js
+++ react-complete-guide/src/components/expense/Expenses.js
@@ -1,20 +1,46 @@
 import "./Expenses.css";
 import Card from "../../shared/Card";
 import ExpenseItem from "./ExpenseItem";
+import ExpenseFilter from "./ExpenseFilter";
+import { useState } from "react";
 
 const Expenses = (props) => {
-  let expensesItems = [];
-  props.expenses.forEach((expense) => {
-    expensesItems.push(
-      <ExpenseItem
-        key={expense.id}
-        title={expense.title}
-        amount={expense.amount}
-        date={expense.date}
+  const [yearFilter, setYearFilter] = useState("2021");
+
+  const renderExpenseItems = () => {
+    let expenses = props.expenses;
+    const expenseItems = [];
+    if (yearFilter !== undefined && yearFilter !== "") {
+      expenses = expenses.filter(
+        (expense) => expense.date.getFullYear().toString() === yearFilter
+      );
+    }
+    expenses.forEach((expense) => {
+      expenseItems.push(
+        <ExpenseItem
+          key={expense.id}
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+        />
+      );
+    });
+    return expenseItems;
+  };
+
+  const expenseYearFilterChangeHandler = (selectedYear) => {
+    setYearFilter(selectedYear);
+  };
+
+  return (
+    <Card className="expenses">
+      <ExpenseFilter
+        selectedYear={yearFilter}
+        onYearFilterChange={expenseYearFilterChangeHandler}
       />
-    );
-  });
-  return <Card className="expenses">{expensesItems}</Card>;
+      {renderExpenseItems()}
+    </Card>
+  );
 };
 
 export default Expenses;
